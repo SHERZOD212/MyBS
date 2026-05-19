4f17d1cf9caa8b4357490a4603bef553138ef049
sherzod <sherzodboss2405@gmail.com>
2026-05-19 18:46:17 +0500

    1 test

---
+++ root/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+from root import settings
+
+urlpatterns = [
+                  # YOUR PATTERNS
+                  path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+                  # Optional UI:
+                  path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+                  path('redoc', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+                  path("admin/", admin.site.urls),
+                  path("api/v1/", include('apps.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
