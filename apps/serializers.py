c35b9b9ab9703e2346bef7ea9d8539326209adfa
sherzod <sherzodboss2405@gmail.com>
2026-06-02 20:11:45 +0500

    Readme

--- apps/serializers.py
+++ apps/serializers.py
@@ -1,10 +1,97 @@
-from rest_framework import serializers
-from .models import Bus, Driver
+# from rest_framework import serializers
+# from .models import Bus, Driver
+#
+# class BusSerializer(ModelSerializer):
+#     # Qo'shimcha ravishda avtobusga biriktirilgan haydovchilar sonini ham ko'rish mumkin
+#     drivers_count = IntegerField(source='drivers.count', read_only=True)
+#
+#     class Meta:
+#         model = Bus
+#         fields = ['id', 'num', 'route', 'drivers_count']
+#
 
-class BusSerializer(serializers.ModelSerializer):
-    # Qo'shimcha ravishda avtobusga biriktirilgan haydovchilar sonini ham ko'rish mumkin
-    drivers_count = serializers.IntegerField(source='drivers.count', read_only=True)
+from datetime import date
 
+from rest_framework.exceptions import ValidationError
+from rest_framework.fields import SerializerMethodField, CharField
+from rest_framework.serializers import ModelSerializer
+
+from .models import Bus, Driver, DailyTripLog, TechnicalStatus, MaintenanceSchedule
+
+
+class BusSerializer(ModelSerializer):
     class Meta:
         model = Bus
-        fields = ['id', 'num', 'route', 'drivers_count']
+        fields = '__all__'
+
+
+class DriverSerializer(ModelSerializer):
+
+    schedule_display = CharField(source='get_schedule_display', read_only=True)
+
+    class Meta:
+        model = Driver
+        fields = '__all__'
+
+
+class DriverDetailSerializer(ModelSerializer):
+
+    bus = BusSerializer(read_only=True)
+    schedule_display = CharField(source='get_schedule_display', read_only=True)
+
+    class Meta:
+        model = Driver
+        fields = '__all__'
+
+
+
+class DailyTripLogSerializer(ModelSerializer):
+    class Meta:
+        model = DailyTripLog
+        fields = '__all__'
+
+
+    def validate(self, attrs):
+        if attrs.get('trips_completed', 0) < 0 or attrs.get('trips_missed', 0) < 0:
+            raise ValidationError("Qatnovlar soni manfiy bo'lishi mumkin emas!")
+        return attrs
+
+
+class DailyTripLogDetailSerializer(ModelSerializer):
+
+    bus = BusSerializer(read_only=True)
+    driver = DriverSerializer(read_only=True)
+
+    class Meta:
+        model = DailyTripLog
+        fields = '__all__'
+
+
+
+class TechnicalStatusSerializer(ModelSerializer):
+    category_display = CharField(source='get_category_display', read_only=True)
+    status_display = CharField(source='get_status_display', read_only=True)
+    km_left = SerializerMethodField()
+
+    class Meta:
+        model = TechnicalStatus
+        fields = '__all__'
+
+    def get_km_left(self, obj):
+
+        return max(0, obj.max_km - obj.current_km)
+
+
+class MaintenanceScheduleSerializer(ModelSerializer):
+
+    class Meta:
+        model = MaintenanceSchedule
+        fields = '__all__'
+
+    def validate(self, attrs):
+
+        planned_date = attrs.get('planned_date')
+        if planned_date:
+            delta = planned_date - date.today()
+            attrs['days_remaining'] = max(0, delta.days)
+        return attrs
--- apps/views.py
+++ apps/views.py
@@ -1,19 +1,59 @@
+# from rest_framework import viewsets, status
+# from rest_framework.decorators import action
+# from rest_framework.response import Response
+# from rest_framework.permissions import IsAuthenticatedOrReadOnly
+# from .models import Bus
+# from .serializers import BusSerializer
+#
+# class BusViewSet(ModelViewSet):
+#     queryset = Bus.objects.all()
+#     serializer_class = BusSerializer
+#     permission_classes = [IsAuthenticatedOrReadOnly] # Rasmdagi qulflar (Authorization) uchun
+#
+#     @action(detail=False, methods=['get'], url_path='active_routes')
+#     def active_routes(self, request):
+#         # Bu yerda o'zingiz xohlagan maxsus filtrni yozasiz
+#         # Masalan: Faqat yo'nalishi bo'sh bo'lmagan avtobuslar
+#         buses = Bus.objects.exclude(route="")
+#         serializer = self.get_serializer(buses, many=True)
+#         return Response(serializer.data)
+#
+#     @action(detail=True, methods=['post'], url_path='fix_route')
+#     def fix_route(self, request, pk=None):
+#         bus = self.get_object()
+#         # Bu yerda maxsus funksiyani bajaramiz (masalan, yo'nalish matniga belgi qo'shish)
+#         bus.route = f"{bus.route} (Tekshirilgan)"
+#         bus.save()
+#         return Response(
+#             {"status": f"{bus.num}-avtobus yo'nalishi yangilandi!"},
+#             status=status.HTTP_200_OK
+#         )
+
+
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .models import Bus
-from .serializers import BusSerializer
+from django.utils import timezone
+from rest_framework.viewsets import ModelViewSet
 
-class BusViewSet(viewsets.ModelViewSet):
+from .models import Bus, Driver, DailyTripLog, TechnicalStatus, MaintenanceSchedule
+from .serializers import (
+    BusSerializer,
+    DriverSerializer, DriverDetailSerializer,
+    DailyTripLogSerializer, DailyTripLogDetailSerializer,
+    TechnicalStatusSerializer,
+    MaintenanceScheduleSerializer
+)
+
+
+class BusViewSet(ModelViewSet):
     queryset = Bus.objects.all()
     serializer_class = BusSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly] # Rasmdagi qulflar (Authorization) uchun
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     @action(detail=False, methods=['get'], url_path='active_routes')
     def active_routes(self, request):
-        # Bu yerda o'zingiz xohlagan maxsus filtrni yozasiz
-        # Masalan: Faqat yo'nalishi bo'sh bo'lmagan avtobuslar
         buses = Bus.objects.exclude(route="")
         serializer = self.get_serializer(buses, many=True)
         return Response(serializer.data)
@@ -21,10 +61,67 @@ class BusViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=['post'], url_path='fix_route')
     def fix_route(self, request, pk=None):
         bus = self.get_object()
-        # Bu yerda maxsus funksiyani bajaramiz (masalan, yo'nalish matniga belgi qo'shish)
         bus.route = f"{bus.route} (Tekshirilgan)"
         bus.save()
         return Response(
             {"status": f"{bus.num}-avtobus yo'nalishi yangilandi!"},
             status=status.HTTP_200_OK
         )
+
+
+class DriverViewSet(ModelViewSet):
+    queryset = Driver.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return DriverDetailSerializer
+
+        return DriverSerializer
+
+    @action(detail=False, methods=['get'], url_path='blacklist')
+    def blacklist(self, request):
+        bad_drivers = Driver.objects.filter(black=True)
+        serializer = self.get_serializer(bad_drivers, many=True)
+        return Response(serializer.data)
+
+
+class DailyTripLogViewSet(ModelViewSet):
+    queryset = DailyTripLog.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return DailyTripLogDetailSerializer
+        return DailyTripLogSerializer
+
+    @action(detail=False, methods=['get'], url_path='today_logs')
+    def today_logs(self, request):
+        today = timezone.now().date()
+        logs = DailyTripLog.objects.filter(date=today)
+        serializer = self.get_serializer(logs, many=True)
+        return Response(serializer.data)
+
+
+class TechnicalStatusViewSet(ModelViewSet):
+    queryset = TechnicalStatus.objects.all()
+    serializer_class = TechnicalStatusSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    @action(detail=False, methods=['get'], url_path='dangerous_buses')
+    def dangerous_buses(self, request):
+        bad_tech = TechnicalStatus.objects.filter(status__in=['danger', 'repair'])
+        serializer = self.get_serializer(bad_tech, many=True)
+        return Response(serializer.data)
+
+
+class MaintenanceScheduleViewSet(ModelViewSet):
+    queryset = MaintenanceSchedule.objects.all()
+    serializer_class = MaintenanceScheduleSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    @action(detail=False, methods=['get'], url_path='upcoming')
+    def upcoming_maintenance(self, request):
+        upcoming = MaintenanceSchedule.objects.filter(days_remaining__lte=7).order_by('days_remaining')
+        serializer = self.get_serializer(upcoming, many=True)
+        return Response(serializer.data)
